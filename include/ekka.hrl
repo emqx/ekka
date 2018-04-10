a02e63b8c8b6f5280055f01c4c5312e710a1921b
Feng Lee <feng@emqtt.io>
2018-04-10 12:38:52 +0800

    Add 'cluster()' type

--- include/ekka.hrl
+++ include/ekka.hrl
@@ -1,4 +1,6 @@
 
+-type(cluster() :: atom()).
+
 -type(member_status() :: joining | up | healing | leaving | down).
 
 -type(member_address() :: {inet:ip_address(), inet:port_number()}).
@@ -9,8 +11,7 @@
                   hash   :: pos_integer(),
                   status :: member_status(),
                   mnesia :: running | stopped | false,
-                  ltime  :: erlang:timestamp()
-                }).
+                  ltime  :: erlang:timestamp() }).
 
 -type(member() :: #member{}).
 
