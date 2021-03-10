e0c92e275cdc47c604b72f9053da3f65cb7913b7
k32 <10274441+k32@users.noreply.github.com>
2021-03-10 18:32:18 +0100

    Add mocks for batch import

--- src/ekka_rlog_replica.erl
+++ src/ekka_rlog_replica.erl
@@ -19,7 +19,7 @@
 -module(ekka_rlog_replica).
 
 %% API:
--export([start_link/1]).
+-export([start_link/1, push_batch/3]).
 
 %% gen_statem callbacks:
 -export([init/1, terminate/3, code_change/4, callback_mode/0, handle_event/4]).
@@ -43,8 +43,9 @@
 
 -record(d,
         { agent                        :: pid()
-        , bootstrap_client = undefined :: pid() | undefined
+        , tmp_worker       = undefined :: pid() | undefined
         , checkpoint       = undefined :: ekka_rlog_server:checkpoint()
+        , next_batch_seqno = 0         :: integer()
         }).
 
 -type data() :: #d{}.
@@ -52,6 +53,11 @@
 %%================================================================================
 %% API funcions
 %%================================================================================
+
+%% This function is called by the remote core node.
+-spec push_batch(node(), ekka_rlog:shard(), ekka_rlog_lib:batch()) -> ok.
+push_batch(Node, Shard, Batch) ->
+    gen_rpc:call(gen_statem, call, [Shard, {tlog_batch, Batch}, infinity]).
 
 start_link(Shard) ->
     Config = #{}, % TODO
@@ -73,14 +79,35 @@ init({Shard, _Opts}) ->
 
 -spec handle_event(gen_statem:event_type(), _EventContent, state(), data()) ->
           gen_statem:event_handler_result(state()).
-%% `disconnected' state:
+%% Handle batches:
+%%   1. Normal flow, transactions are applied directly to the replica:
+handle_event(call, {tlog_batch, {Agent, SeqNo, Transactions}}, ?normal,
+             #d{agent = Agent, next_batch_seqno = SeqNo}) ->
+    ekka_rlog_lib:import_batch(Batch),
+    {keep_state, D#d{next_batch_seqno = SeqNo + 1}, [{reply, ok}]};
+%%   2. Bootstrapping is in progress, transactions are being buffered:
+handle_event(call, {tlog_batch, {Agent, SeqNo, Transactions}}, St,
+             #d{agent = Agent, next_batch_seqno = SeqNo})
+  when St =:= ?bootstrap orelse St =:= ?local_replay ->
+    buffer_tlog_ops(Transactions),
+    {keep_state, D#d{next_batch_seqno = SeqNo + 1}, [{reply, ok}]};
+%%   3. Gap in the TLOG, we don't support that, crash and restart:
+handle_event(call, {tlog_batch, {Agent, SeqNo, _}}, _,
+             #d{agent = Agent, next_batch_seqno = MySeqNo})
+  when SeqNo > MySeqNo ->
+    error(gap_in_the_tlog);
+%% Events specific for `disconnected' state:
 handle_event(enter, _, ?disconnected, D) ->
     {keep_state_and_data, [{timeout, 0, reconnect}]};
 handle_event(timeout, reconnect, ?disconnected, D) ->
     handle_reconnect(D);
-%% `bootstrap' state:
-%% `local_replay' state:
-%% `normal' state:
+%% Events specific for `bootstrap' state:
+handle_event(cast, {bootstrap_complete, Pid, Checkpoint}, ?bootstrap, D = #d{tmp_worker = Pid}) ->
+    {next_state, ?local_replay, D#d{ tmp_worker = undefined
+                                   , checkpoint = Checkpoint
+                                   }};
+%% Events specific for `local_replay' state:
+%% Events specific for `normal' state:
 %% Common actions:
 handle_event(enter, OldState, State, Data) ->
     handle_state_trans(OldState, State, Data),
@@ -99,13 +126,14 @@ terminate(_Reason, _State, _Data) ->
 %% Internal functions
 %%================================================================================
 
+%% @private Try connecting to a core node
 -spec handle_reconnect(data()) -> gen_statem:event_handler_result(state()).
 handle_reconnect(D) ->
     case try_connect(D#d.checkpoint) of
         {ok, _BootstrapNeeded = true, ConnPid} ->
             {next_state, ?bootstrap, #d{agent = ConnPid}};
         {ok, _BootstrapNeeded = false, ConnPid} ->
-            {next_state, ?normal, D#d{agent = ConnPid}};
+            {next_state, ?normal, #d{agent = ConnPid, checkpoint = D#d.checkpoint}};
         {error, Err} ->
             ReconnectTimeout = application:get_env(ekka, rlog_replica_reconnect_interval, 5000),
             {keep_state_and_data, [{timeout, ReconnectTimeout, reconnect}]}
@@ -114,6 +142,10 @@ handle_reconnect(D) ->
 -spec try_connect(ekka_rlog_server:checkpoint()) -> {ok, boolean(), pid()} | {error, term()}.
 try_connect(Checkpoint) ->
     {error, not_implemented}.
+
+-spec buffer_tlog_ops([ekka_rlog_lib:tx()], data()) -> ok.
+buffer_tlog_ops(Batch, Data) ->
+    ok. %% TODO
 
 handle_unknown(EventType, Event, State, Data) ->
     ?tp(warning, "rlog agent received unknown event",
