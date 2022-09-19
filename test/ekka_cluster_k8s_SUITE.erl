d562862f8b600b935d7f09e0b5d4eb5caeabab36
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2022-09-19 10:36:28 +0200

    refactor(strategy): Wrap callback functions

--- test/ekka_cluster_k8s_SUITE.erl
+++ test/ekka_cluster_k8s_SUITE.erl
@@ -1,5 +1,5 @@
 %%--------------------------------------------------------------------
-%% Copyright (c) 2019 EMQ Technologies Co., Ltd. All Rights Reserved.
+%% Copyright (c) 2019-2022 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -36,9 +36,9 @@ t_discover(_) ->
     ok = meck:expect(httpc, request, fun(get, _Req, _Opts, _) ->
                                              {ok, {{"HTTP/1.1", 200, "OK"}, [], Json}}
                                      end),
-    {ok, ['ekka@192.168.10.10']} = ekka_cluster_k8s:discover([{app_name, "ekka"} | ?OPTIONS]),
+    {ok, ['ekka@192.168.10.10']} = ekka_cluster_strategy:discover(ekka_cluster_k8s, [{app_name, "ekka"} | ?OPTIONS]),
     %% below test relies on rebar3 ct is run with '--name ct@127.0.0.1'
-    {ok, ['ct@192.168.10.10']} = ekka_cluster_k8s:discover(?OPTIONS),
+    {ok, ['ct@192.168.10.10']} = ekka_cluster_strategy:discover(ekka_cluster_k8s, ?OPTIONS),
     ok = meck:unload(httpc).
 
 t_lock(_) ->
@@ -52,4 +52,3 @@ t_register(_) ->
 
 t_unregister(_) ->
     ignore = ekka_cluster_static:unregister([]).
-
